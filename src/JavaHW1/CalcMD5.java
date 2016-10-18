93ab95b81dcec9f2eda3a7a67ead1fd95070d111
vrom911 <vrom911@gmail.com>
2016-10-18 15:11:51 +0300

    Read to ByteArray and Deal with Exception

--- src/JavaHW1/CalcMD5.java
+++ src/JavaHW1/CalcMD5.java
@@ -2,44 +2,38 @@ package JavaHW1;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+
 /**
  * Created by vrom911 on 10/17/16.
  */
-public class JavaHW1 {
+public class CalcMD5 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java JavaCourse input.txt");
+            return;
+        }
+        solveHW1(args[0]);
+    }
 
-    public  static void solveHW1(String f) {
+    public static void solveHW1(String f) {
         try (BufferedReader inputBuf = new BufferedReader(new FileReader(new File("JavaHW1/" + f)))){
-            System.out.println("hmm");
             MessageDigest md = MessageDigest.getInstance("MD5");
             String fileName;
             while ((fileName = inputBuf.readLine()) != null && fileName.length() != 0) {
 
                 System.out.println(calcFileMD5(md, "JavaHW1/" + fileName));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
         }
     }
-    private static String calcFileMD5(MessageDigest md, String fPath) {
-        Path path = Paths.get(fPath);
-        byte[] fileBArray = new byte[0];
-        try {
-            fileBArray = Files.readAllBytes(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(fileBArray);
+    private static String calcFileMD5(MessageDigest md, String fPath) throws IOException {
         // hashing the file content converted to bytecode
-        byte[] mdbytes = md.digest(fileBArray);
+        byte[] mdbytes = md.digest(Files.readAllBytes(Paths.get(fPath)));
         // convert to hex before returning
         return javax.xml.bind.DatatypeConverter.printHexBinary(mdbytes);
     }
