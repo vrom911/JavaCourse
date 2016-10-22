8ead88ee7f830442a0fc41935e3a1aa4b44d51d7
vrom911 <vrom911@gmail.com>
2016-10-23 00:27:09 +0300

    Reformat code

--- src/JavaHW2/BinarySearch.java
+++ src/JavaHW2/BinarySearch.java
@@ -8,24 +8,26 @@ public class BinarySearch {
         int x = Integer.parseInt(args[0]);
         int[] m = new int[args.length - 1];
         for (int i = 0; i < args.length - 1; i++) {
-            m[i] = Integer.parseInt(args[i+1]);
+            m[i] = Integer.parseInt(args[i + 1]);
         }
 //        System.out.println(normalSearch(m, x));
         System.out.println(recursiveSearch(m, x, 0, m.length));
     }
 
     public static  int normalSearch(int[] m, int x) {
-        int idxL = 0, idxR = m.length ;
-        int mid;
+        int idxL = 0;
+        int idxR = m.length;
         while (idxL + 1 < idxR) {
-            mid = (idxL + idxR) / 2;
+            int mid = (idxL + idxR) / 2;
             if (m[mid] > x) {
                 idxL = mid;
             } else {
                 idxR = mid;
             }
         }
-        if (idxL+1 == idxR &&  m[idxL] <= x) {idxR = idxL;}
+        if (idxL + 1 == idxR && m[idxL] <= x) {
+            idxR = idxL;
+        }
         return idxR;
     }
     public static int recursiveSearch(int[] m, int x, int idxL, int idxR) {
@@ -37,9 +39,10 @@ public class BinarySearch {
                 return recursiveSearch(m, x, idxL, mid);
             }
         } else {
-            if(idxL+1 == idxR && m[idxL] <= x ) {idxR = idxL;}
+            if (idxL + 1 == idxR && m[idxL] <= x) {
+                idxR = idxL;
+            }
             return idxR;
         }
-
     }
 }
